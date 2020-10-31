3e367422123a82663dbefcc78434443f777b11cb
cargovova <kuw3esoL>
2020-10-31 18:55:36 +0400

    New instance multisearchConnection class for every function in dbQuery

--- src/db/dbQuery.php
+++ src/db/dbQuery.php
@@ -8,16 +8,11 @@ namespace src\db;
  */
 class dbQuery
 {
-    private $connection;
-    public function __construct()
-    {
-        $this->connection = new multisearchConnection();
-    }
-
     // выбрать всё. Используется при первоначальной загрузке страницы.
     public function select()
     {
-        $result = $this->connection->query('SELECT * FROM names ORDER BY name;');
+        $connection = new multisearchConnection();
+        $result = $connection->query('SELECT * FROM names ORDER BY name;');
         $commonArray = [];
         // fetchArray выдает одну строку только, а while помещает все строки в массив
         while ($arrays = $result->fetchArray(SQLITE3_ASSOC)) {
@@ -29,11 +24,14 @@ class dbQuery
     // вставка нового значения в базу.
     public function insert($text)
     {
-        $this->connection = $this->connection->query("INSERT INTO names (name) VALUES ('" . $text . "');");
+        $connection = new multisearchConnection();
+        $connection->query("INSERT INTO names (name) VALUES ('" . $text . "');");
     }
 
     // удаление строки из бд
     public function delete($text){
-        $this->connection = $this->connection->query("DELETE FROM names WHERE name=('" . $text . "');");
+        $connection = new multisearchConnection();
+        $connection->query("DELETE FROM names WHERE name=('" . $text . "');");
     }
 }
+
